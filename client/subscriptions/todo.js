4b1e7587fbc14a0aee1f02e276e8fc85e532fe48
Joe <joebrotzer@lanex-gian.local>
2019-03-22 18:08:45 +0800

    edited hasura calls

--- client/subscriptions/todo.js
+++ client/subscriptions/todo.js
@@ -2,13 +2,14 @@ import gql from 'graphql-tag';
 
 /**
  * Get the list of todos
+ * 
+ * @returns todos
  */
 export const SUBSCRIPTION_TODOS = gql`
 	subscription Todos {
 		todos(order_by: { id: asc }) {
 			id
-			value
-			is_done
+			todo
 		}
 	}
 `;
